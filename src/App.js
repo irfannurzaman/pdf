3ff545b47fa2121cf8b16681a707b20e41746ad7
irfan nurzaman <inurzmn@gmail.com>
2024-03-03 19:19:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { pdfjs } from "react-pdf";
 import FileUpload from "./components/FileUpload";
-import Example from "./example"
-import SideBar from './components/SideBar';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
@@ -15,8 +13,6 @@ function App() {
   return (
     <div>
       <FileUpload/>
-      {/* <Example/> */}
-      
     </div>
   );
 }
